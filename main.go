19913897f32970c582a6a204ac746a01e284393e
fangyuhao <fangyuhao@xiaomi.com>
2017-07-18 09:01:41 +0800

    succ to call docker client

--- main.go
+++ main.go
@@ -9,23 +9,32 @@ import (
 	"github.com/docker/docker/client"
 )
 
+const (
+	Nanosecond  time.Duration = 1
+	Microsecond               = 1000 * Nanosecond
+	Millisecond               = 1000 * Microsecond
+	Second                    = 1000 * Millisecond
+	Minute                    = 60 * Second
+	Hour                      = 60 * Minute
+)
+
 func main() {
 	cli, err := client.NewEnvClient()
 	if err != nil {
 		panic(err)
 	}
-	cli.SetTimeout(1 * Minute)
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-	if err != nil {
-		panic(err)
-	}
 
-	for _, container := range containers {
-		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
-	}
 	go func() {
-		for _ = range time.Tick(2 * Minute) {
+		for _ = range time.Tick(2 * Second) {
+			containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+			if err != nil {
+				panic(err)
+			}
 
+			for _, container := range containers {
+				fmt.Printf("%s %s\n", container.ID[:10], container.Image)
+			}
 		}
 	}()
+	select {}
 }
